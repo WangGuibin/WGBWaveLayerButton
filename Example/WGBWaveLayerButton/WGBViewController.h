f4a5bd9a50f898d8133479ea04fbe954bccf24fa
YGC <guchengye153@gmail.com>
2019-09-18 20:56:44 +0700

    Initial commit

---
+++ Example/WGBWaveLayerButton/WGBViewController.h
@@ -0,0 +1,13 @@
+//
+//  WGBViewController.h
+//  WGBWaveLayerButton
+//
+//  Created by Wangguibin on 09/18/2019.
+//  Copyright (c) 2019 Wangguibin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WGBViewController : UIViewController
+
+@end
