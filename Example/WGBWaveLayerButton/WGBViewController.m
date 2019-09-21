1066c67d7d592175897ae13501e2c76dfaa3f93f
YGC <guchengye153@gmail.com>
2019-09-21 16:28:41 +0700

    update~

--- Example/WGBWaveLayerButton/WGBViewController.m
+++ Example/WGBWaveLayerButton/WGBViewController.m
@@ -28,28 +28,36 @@
     [waveButton setTitle:@"Click Me" forState:UIControlStateNormal];
     [self.view addSubview:waveButton];
     
+    // 在app里获取git提交记录 参考学习https://github.com/y500/GetGitInfo
     NSDictionary *infoDic = [[NSBundle mainBundle] infoDictionary];
-    
+
     CGFloat offsetY = CGRectGetMaxY(waveButton.frame) + 30;
     UILabel *commitLabel = [self createLabelWithOffsetY:offsetY];
     commitLabel.text = [NSString stringWithFormat:@"最后提交SHA：%@", [infoDic objectForKey:@"GitCommitSHA"]];
     
-    offsetY = CGRectGetMaxY(commitLabel.frame) + 30;
+    offsetY = CGRectGetMaxY(commitLabel.frame);
     UILabel *branchLabel = [self createLabelWithOffsetY:offsetY];
     branchLabel.text = [NSString stringWithFormat:@"当前所在分支：%@", [infoDic objectForKey:@"GitCommitBranch"]];
     
-     offsetY = CGRectGetMaxY(branchLabel.frame) + 30;
+     offsetY = CGRectGetMaxY(branchLabel.frame);
     UILabel *authorLabel = [self createLabelWithOffsetY:offsetY];
     authorLabel.text = [NSString stringWithFormat:@"最后提交用户：%@", [infoDic objectForKey:@"GitCommitUser"]];
     
-    offsetY = CGRectGetMaxY(authorLabel.frame) + 30;
+    offsetY = CGRectGetMaxY(authorLabel.frame);
     UILabel *dateLabel = [self createLabelWithOffsetY:offsetY];
     dateLabel.text = [NSString stringWithFormat:@"最后提交时间：%@", [infoDic objectForKey:@"GitCommitDate"]];
     
-    offsetY = CGRectGetMaxY(dateLabel.frame) + 30;
+    offsetY = CGRectGetMaxY(dateLabel.frame);
     UILabel *buildTimeLabel = [self createLabelWithOffsetY:offsetY];
     buildTimeLabel.text = [NSString stringWithFormat:@"本次编译时间：%@", [infoDic objectForKey:@"BuildTime"]];
-        
+   
+    offsetY = CGRectGetMaxY(buildTimeLabel.frame) + 20;
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(30, offsetY, self.view.bounds.size.width - 60 , 200)];
+    [self.view addSubview: textView];
+    
+    textView.editable = NO;
+    textView.text = [NSString stringWithFormat:@"Git提交日志：%@", [infoDic objectForKey:@"GitLog"]];
+
 }
 
 - (UILabel *)createLabelWithOffsetY:(CGFloat)offsetY {
